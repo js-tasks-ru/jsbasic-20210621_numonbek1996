c1f54a96ab9790c87165c2e26291cfe98556cdbf
Numonbek Tirkashev <tirkashevco@gmail.com>
2021-06-28 11:27:50 +0300

    исправил ошибки

--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -3,14 +3,9 @@ function print(text) {
 }
 
 function isValid(name) {
-<<<<<<< HEAD
   // if (name !== "" && name !== "  " && name.length >= 4) {
   //   return name;
-    return !!name && !name.includes(' ') && name.length >= 4;  //код приподователя
-  }
-=======
-  return !!name && !name.includes(' ') && name.length >= 4;
->>>>>>> dc0690e756e73a16cd05612d2ed09185b4fd6521
+  return !!name && !name.includes(" ") && name.length >= 4; //код приподователя
 }
 
 function sayHello() {
