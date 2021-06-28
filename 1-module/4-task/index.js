7ce80892ac28c914c43759cca5e9d779ce503d3b
Numonbek Tirkashev <tirkashevco@gmail.com>
2021-06-28 12:50:27 +0300

    changes

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,5 +1,8 @@
 function checkSpam(str) {
   let strLowerCase = str.toLowerCase();
 
-  return strLowerCase.includes('1xbet') || strLowerCase.includes('xxx');
+  return strLowerCase.includes("1xbet") || strLowerCase.includes("xxx");
 }
+checkSpam("1XbeT now");
+checkSpam("free xxxxx");
+checkSpam("innocent rabbit");
