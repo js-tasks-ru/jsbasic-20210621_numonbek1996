8ef816ee4fe930b357043103dee9d209310aa1fc
Numonbek Tirkashev <tirkashevco@gmail.com>
2021-07-01 18:31:43 +0300

    функция которий я написал у меня работал без ошибки почему не пропускает проверка

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -1,6 +1,12 @@
 function ucFirst(str) {
-  if (!str && !str[0].includes(" ")) {
-    return str;
-  }
-  return str[0].toUpperCase() + str.slice(1);
+  // if (!str && !str[0].includes(" ")) {
+  //   return str;
+  // }
+  // return str[0].toUpperCase() + str.slice(1);
+
+  let firstLetter = str.charAt(0);
+  let firstLetterUpperCase = firstLetter.toUpperCase();
+  let otherLetters = str.slice(1);
+
+  return `${firstLetterUpperCase}${otherLetters}`;
 }
