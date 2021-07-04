e021f19bc7ae90d61e898b2c9a3d225529d73194
Numonbek Tirkashev <tirkashevco@gmail.com>
2021-07-04 11:09:24 +0300

    если есть другой более сокращений способ написания кода напишите пожалуйста

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,8 @@
 function camelize(str) {
-  // ваш код...
+  let arr = str.split("-");
+  let result = arr.map(
+    (item) => item.slice(0, 1).toUpperCase() + item.slice(1)
+  );
+  let connect = result.join("");
+  return connect;
 }
